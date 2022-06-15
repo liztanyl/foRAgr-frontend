35dcfdb9b7f64cb12875ebafdf5bc2251350a0dc
gcskhor <gcskhor@yahoo.com.sg>
2022-06-15 17:24:07 +0800

    Added EnterReceipt component

--- components/TabScreens/EnterReceipt.js
+++ components/TabScreens/EnterReceipt.js
@@ -1,6 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Box, Button, VStack } from 'native-base';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function EnterReceipt() {
-	return <Text>EnterReceipt</Text>;
+	return (
+		<Box>
+			<VStack space={3} alignItems="center">
+				<Button
+					size="lg"
+					padding={10}
+					colorScheme="primary"
+					rightIcon={<FontAwesome name="camera" size={24} color="white" />}
+					onPress={() => console.log('go to camera mode')}
+				>
+					Camera Mode
+				</Button>
+				<Button
+					size="lg"
+					padding={10}
+					colorScheme="secondary"
+					rightIcon={
+						<FontAwesome name="pencil-square-o" size={24} color="white" />
+					}
+					onPress={() => console.log('go to manual entry mode')}
+				>
+					Manual Entry
+				</Button>
+			</VStack>
+		</Box>
+	);
 }
