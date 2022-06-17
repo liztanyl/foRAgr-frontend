5d015c00104ab2aa73c36ac0bd1d94ce11b3eaf7
cheenaeng <cheena94sing@gmail.com>
2022-06-17 10:38:23 +0800

    ip changes

--- store.js
+++ store.js
@@ -1,22 +1,23 @@
-export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3004';
+import myIpAddress from './myIpAddress.js';
+export const BACKEND_URL = process.env.BACKEND_URL || myIpAddress;
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // LOCAL STORAGE FUNCTIONS
 export const storeData = async (key, value) => {
-	try {
-		const jsonValue = JSON.stringify(value);
-		await AsyncStorage.setItem(key, jsonValue);
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getData = async (key) => {
-	try {
-		const jsonValue = await AsyncStorage.getItem(key);
-		return jsonValue != null ? JSON.parse(jsonValue) : null;
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (err) {
+    console.log(err);
+  }
 };
