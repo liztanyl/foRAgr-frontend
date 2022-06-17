dad60e38c8bca5868a7c277df6560f4d6d367252
tabbiho <tabbiho@gmail.com>
2022-06-17 10:36:55 +0800

    updated backend url to system ip address

--- store.js
+++ store.js
@@ -1,22 +1,3 @@
-export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3004';
+import myIpAddress from "./myIpAddress.js";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// LOCAL STORAGE FUNCTIONS
-export const storeData = async (key, value) => {
-	try {
-		const jsonValue = JSON.stringify(value);
-		await AsyncStorage.setItem(key, jsonValue);
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-export const getData = async (key) => {
-	try {
-		const jsonValue = await AsyncStorage.getItem(key);
-		return jsonValue != null ? JSON.parse(jsonValue) : null;
-	} catch (err) {
-		console.log(err);
-	}
-};
+export const BACKEND_URL = process.env.BACKEND_URL || `http://${myIpAddress}:3004`;
