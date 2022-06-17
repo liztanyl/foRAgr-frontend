2b9ba52ed269af8c0680df6d2b4259a590489ad6
liztanyl <elizabeth.tanyulin@gmail.com>
2022-06-17 09:04:29 +0800

    update fridge context to include fridge reducer also refactored fridge context provider and its exports

--- components/FridgeContext.js
+++ components/FridgeContext.js
@@ -7,85 +7,183 @@ const STORAGE_KEYS = {
 };
 
 const ACTIONS = {
-	RETRIEVE: 'retrieve items',
-	ADD_ITEMS: 'add items',
-	REMOVE_ITEM: 'remove item',
-	EDIT_ITEM: 'edit item', // can remove if not used
+	// for review reducer
+	REVIEW: {
+		RETRIEVE: 'retrieve items for review',
+		ADD_ITEMS: 'add items to review',
+		REMOVE_ITEM: 'remove item from review',
+	},
+
+	// for fridge reducer
+	FRIDGE: {
+		RETRIEVE: 'retrieve items from fridge',
+		ADD_ITEMS: 'add items to fridge',
+		REMOVE_ITEM: 'remove item from fridge',
+	},
 };
 
-export function reviewReducer(state, action) {
+// ************************************
+// --------- DISPATCH HELPERS ---------
+// ************************************
+
+const addReviewItems = (newItemsIds) => {
+	console.log('review - ids to add', newItemsIds);
+	return {
+		type: ACTIONS.REVIEW.ADD_ITEMS,
+		payload: newItemsIds,
+	};
+};
+
+const removeReviewItem = (idToRemove) => {
+	console.log('review - id to remove', idToRemove);
+	return {
+		type: ACTIONS.REVIEW.REMOVE_ITEM,
+		payload: idToRemove,
+	};
+};
+
+const addFridgeItems = (newItems) => {
+	console.log('fridge - items to add', newItems);
+	return {
+		type: ACTIONS.FRIDGE.ADD_ITEMS,
+		payload: newItems,
+	};
+};
+
+const removeFridgeItem = (idToRemove) => {
+	console.log('fridge - id to remove', idToRemove);
+	return {
+		type: ACTIONS.FRIDGE.REMOVE_ITEM,
+		payload: idToRemove,
+	};
+};
+
+// ************************************
+// -------- REDUCER FUNCTIONS ---------
+// ************************************
+
+const reviewReducer = (state, action) => {
 	switch (action.type) {
-		case ACTIONS.RETRIEVE:
+		case ACTIONS.REVIEW.RETRIEVE:
+			const ids = action.payload;
+			console.log('review reducer-retrieve state', state);
+			if (state) {
+				return [...state, ...ids];
+			} else {
+				return ids;
+			}
+
+		case ACTIONS.REVIEW.ADD_ITEMS:
+			console.log('review reducer-add state', state);
+			const newItems = state ? [...state, ...action.payload] : action.payload;
+			AsyncStorage.setItem(STORAGE_KEYS.REVIEW, JSON.stringify(newItems));
+			return newItems;
+
+		case ACTIONS.REVIEW.REMOVE_ITEM:
+			if (state) {
+				const idToRemove = action.payload;
+				const updatedItems = state.filter((id) => id !== idToRemove);
+				AsyncStorage.setItem(STORAGE_KEYS.REVIEW, JSON.stringify(updatedItems));
+				return updatedItems;
+			} else break;
+
+		default:
+			console.log('error in review reducer', state, action);
+			throw new Error();
+	}
+};
+
+const fridgeReducer = (state, action) => {
+	switch (action.type) {
+		case ACTIONS.FRIDGE.RETRIEVE:
 			const items = action.payload;
-			console.log('reducer-retrieve state', state);
+			console.log('fridge reducer-retrieve state', state);
 			if (state) {
 				return [...state, ...items];
 			} else {
 				return items;
 			}
-		case ACTIONS.ADD_ITEMS:
-			console.log('reducer-add state', state);
-			const newItems = state ? [...state, ...action.payload] : action.payload;
-			AsyncStorage.setItem(STORAGE_KEYS.REVIEW, JSON.stringify(newItems));
-			return newItems;
-		// case ACTIONS.REMOVE_ITEM:
-		// 	console.log(state);
-		// 	return { ...state, reviewItemIds: action.payload };
-		// case ACTIONS.EDIT_ITEM:
-		// return { ...state, reviewItemIds: action.payload };
+
+		case ACTIONS.FRIDGE.ADD_ITEMS:
+			console.log('fridge reducer-add state', state);
+			const newFridge = state ? [...state, ...action.payload] : action.payload;
+			AsyncStorage.setItem(STORAGE_KEYS.FRIDGE, JSON.stringify(newFridge));
+			return newFridge;
+
+		case ACTIONS.FRIDGE.REMOVE_ITEM:
+			if (state) {
+				const idToRemove = action.payload;
+				const updatedFridge = state.filter((item) => item.id !== idToRemove);
+				AsyncStorage.setItem(
+					STORAGE_KEYS.FRIDGE,
+					JSON.stringify(updatedFridge)
+				);
+				return updatedFridge;
+			} else break;
 
 		default:
-			console.log('error', state, action);
+			console.log('error in fridge reducer', state, action);
 			throw new Error();
 	}
-}
-
-// DISPATCH HELPERS
-const addReviewItems = (newItems) => {
-	console.log(newItems);
-	return {
-		type: ACTIONS.ADD_ITEMS,
-		payload: newItems,
-	};
 };
 
-const removeReviewItems = async (idToRemove) => {
-	// const reviewItems = await retrieveItemsLS();
-	// const updatedReviewItems = reviewItems.filter((id) => id !== idToRemove);
-	// updateItemsLS(updatedReviewItems);
-	// return {
-	// 	type: ACTIONS.REMOVE_ITEM,
-	// 	payload: updatedReviewItems,
-	// };
-};
+// ************************************
+// -------- CONTEXT & PROVIDER --------
+// ************************************
 
-export const FridgeContext = React.createContext(null);
+// Seed items in AsyncStorage to test reducer functions
+// ** TODO: remove in deployment
+AsyncStorage.clear();
+AsyncStorage.setItem(STORAGE_KEYS.REVIEW, JSON.stringify([1, 2, 3]), () =>
+	console.log('done seeding review item ids in asyncstorage: [1,2,3]')
+);
+AsyncStorage.setItem(
+	STORAGE_KEYS.FRIDGE,
+	JSON.stringify([{ id: 12, name: 'apricots' }]),
+	() => console.log('done seeding fridge items in asyncstorage')
+);
 
+const FridgeContext = React.createContext(null);
+
+// Call the below function when you want to use the FridgeContext
+// eg, use --> const { reviewItemIds } = useFridgeContext()
+// instead of: const { reviewItemIds } = useContext(FridgeContext)
 export function useFridgeContext() {
 	return useContext(FridgeContext);
 }
 
-AsyncStorage.setItem(STORAGE_KEYS.REVIEW, JSON.stringify([1, 2, 3]), () =>
-	console.log('done seeding item ids in asyncstorage: [1,2,3]')
-);
-
 export function FridgeContextProvider({ children }) {
 	const [reviewItemIds, reviewDispatch] = useReducer(reviewReducer, null);
-	// const [fridgeItems, fridgeItemsDispatch] = useReducer(fridgeReducer, []);
+	const [fridgeItems, fridgeDispatch] = useReducer(fridgeReducer, null);
 
-	// TODO: REDUCER FOR FRIDGE ITEMS
-
-	const retrieveItemsFromAsyncStorage = () => {
+	const getReviewItemsFromAsyncStorage = () => {
 		AsyncStorage.getItem(STORAGE_KEYS.REVIEW, (err, result) => {
 			if (result) {
-				console.log('<<<<<< retrieveItemsFromAsyncStorage');
+				console.log('<<<<<< getReviewItemsFromAsyncStorage');
 				console.log('result', result);
 				console.log('------------------------- >>>>>>>>>>');
 				reviewDispatch({
-					type: ACTIONS.RETRIEVE,
+					type: ACTIONS.REVIEW.RETRIEVE,
 					payload: JSON.parse(result),
 				});
-				console.log('<<<<<< retrieveItemsFromAsyncStorage');
+				console.log('<<<<<< getReviewItemsFromAsyncStorage');
+				console.log('reviewItemIds', reviewItemIds);
+				console.log('------------------------- >>>>>>>>>>');
+			}
+		});
+	};
+
+	const getFridgeItemsFromAsyncStorage = () => {
+		AsyncStorage.getItem(STORAGE_KEYS.FRIDGE, (err, result) => {
+			if (result) {
+				console.log('<<<<<< getFridgeItemsFromAsyncStorage');
+				console.log('result', result);
+				console.log('------------------------- >>>>>>>>>>');
+				fridgeDispatch({
+					type: ACTIONS.FRIDGE.RETRIEVE,
+					payload: JSON.parse(result),
+				});
+				console.log('<<<<<< getFridgeItemsFromAsyncStorage');
 				console.log('reviewItemIds', reviewItemIds);
 				console.log('------------------------- >>>>>>>>>>');
 			}
@@ -93,17 +191,24 @@ export function FridgeContextProvider({ children }) {
 	};
 
 	useEffect(() => {
-		retrieveItemsFromAsyncStorage();
+		getReviewItemsFromAsyncStorage();
+		getFridgeItemsFromAsyncStorage();
 	}, []);
 
-	useEffect(() => console.log('reviewItemIds', reviewItemIds), [reviewItemIds]);
+	useEffect(() => {
+		console.log('reviewItemIds', reviewItemIds);
+		console.log('fridgeItems', fridgeItems);
+	}, [reviewItemIds, fridgeItems]);
 
 	return (
 		<FridgeContext.Provider
 			value={{
 				reviewItemIds,
 				reviewDispatch,
-				reviewDispatchHelpers: [addReviewItems, removeReviewItems],
+				reviewDispatchHelpers: [addReviewItems, removeReviewItem],
+				fridgeItems,
+				fridgeDispatch,
+				fridgeDispatchHelpers: [addFridgeItems, removeFridgeItem],
 			}}
 		>
 			{children}
