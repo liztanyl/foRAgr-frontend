fcb44fa9183b4c2b6d2ae6ac31b93511505d77e9
gcskhor <gcskhor@yahoo.com.sg>
2022-06-18 10:39:04 +0800

    updated itemReview components

--- components/Receipts/ItemReview/ItemForm.jsx
+++ components/Receipts/ItemReview/ItemForm.jsx
@@ -4,51 +4,63 @@ import { StyleSheet } from 'react-native';
 import CategorySelector from './CategorySelector';
 import StorageSelector from './StorageSelector';
 
-export const allSameCategory = (shelfLifeItems) => {
-	let areAllSameCategory = true;
-	const firstCategoryName = shelfLifeItems[0].categoryName;
-	shelfLifeItems.forEach((i) => {
-		if (firstCategoryName != i.categoryName) {
-			areAllSameCategory = false;
-		}
-	});
-	console.log(areAllSameCategory);
-	return areAllSameCategory;
-};
+// export const allSameCategory = (shelfLifeItems) => {
+// 	let areAllSameCategory = true;
+// 	const firstCategoryName = shelfLifeItems[0].categoryName;
+// 	shelfLifeItems.forEach((i) => {
+// 		if (firstCategoryName != i.categoryName) {
+// 			areAllSameCategory = false;
+// 		}
+// 	});
+// 	console.log(areAllSameCategory);
+// 	return areAllSameCategory;
+// };
 
 export default function ItemForm({ item }) {
-	const { name, shelfLifeItems } = item;
-	const [selectedShelfLifeItem, setSelectedShelfLifeItem] = useState();
+  const { name, categories } = item;
+  const [selectedCategory, setSelectedCategory] = useState();
+  const [selectedStorage, setSelectedStorage] = useState();
 
-	const styles = StyleSheet.create({
-		itemContainer: {
-			color: 'black',
-			backgroundColor: 'azure',
-			padding: 10,
-			margin: 10,
-		},
-		itemName: {
-			color: 'black',
-			backgroundColor: 'aliceBlue',
-			padding: 10,
-			margin: 10,
-		},
-	});
+  const styles = StyleSheet.create({
+    itemContainer: {
+      color: 'black',
+      backgroundColor: 'azure',
+      padding: 10,
+      margin: 10,
+    },
+    itemName: {
+      color: 'black',
+      backgroundColor: 'aliceBlue',
+      padding: 10,
+      margin: 10,
+    },
+  });
 
-	return (
-		<Box style={styles.itemContainer}>
-			<Box style={styles.itemName}>{name}</Box>
-			<CategorySelector
-				shelfLifeItems={shelfLifeItems}
-				selectedShelfLifeItem={selectedShelfLifeItem}
-				setSelectedShelfLifeItem={setSelectedShelfLifeItem}
-			/>
-			{selectedShelfLifeItem && (
-				<StorageSelector
-					shelfLifeItems={shelfLifeItems}
-					selectedShelfLifeItem={selectedShelfLifeItem}
-				/>
-			)}
-		</Box>
-	);
+  useEffect(() => {
+    if (categories.length == 1) {
+      setSelectedCategory(categories[0]);
+    }
+    if (selectedCategory) {
+      // console.log(selectedCategory);
+    }
+  }, [selectedCategory]);
+
+  return (
+    <Box style={styles.itemContainer}>
+      <Box style={styles.itemName}>{name}</Box>
+      <CategorySelector
+        categories={categories}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      {selectedCategory && (
+        <StorageSelector
+          categories={categories}
+          selectedCategory={selectedCategory}
+          selectedStorage={selectedStorage}
+          setSelectedStorage={setSelectedStorage}
+        />
+      )}
+    </Box>
+  );
 }
--- components/Receipts/ItemReview/StorageSelector.jsx
+++ components/Receipts/ItemReview/StorageSelector.jsx
@@ -1,49 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Select, Center, FormControl } from 'native-base';
 
-import { allSameCategory } from './ItemForm';
+// import { allSameCategory } from './ItemForm';
 
 export default function StorageSelector({
-	selectedShelfLifeItem,
-	shelfLifeItems,
+  categories,
+  selectedCategory,
+  selectedStorage,
+  setSelectedStorage,
 }) {
-	const [selectedStorage, setSelectedStorage] = useState('');
+  const { storageMethods } = selectedCategory;
 
-	useEffect(() => {
-		console.log(selectedShelfLifeItem);
-		console.log(shelfLifeItems);
-		if (selectedShelfLifeItem) {
-			setSelectedStorage(selectedShelfLifeItem.storageName);
-		}
-	}, [selectedShelfLifeItem]);
-	return (
-		<Box w="3/4" maxW="300">
-			<FormControl isRequired>
-				<Select
-					minWidth="200"
-					placeholder="Choose a storage method"
-					_selectedItem={{
-						bg: 'teal.600',
-					}}
-					mt={1}
-					onValueChange={(itemValue) => {
-						handleValueChange(itemValue);
-					}}
-					defaultValue={selectedStorage ? selectedStorage : null}
-				>
-					{allSameCategory(shelfLifeItems)
-						? shelfLifeItems.map((item, index) => {
-								return (
-									<Select.Item
-										label={item.storageName}
-										value={item.storageName}
-										key={item.storageName + index}
-									/>
-								);
-						  })
-						: selectedShelfLifeItem.storageName}
-				</Select>
-			</FormControl>
-		</Box>
-	);
+  useEffect(() => {
+    if (storageMethods.length == 1) {
+      setSelectedStorage(storageMethods[0]);
+    }
+  }, [selectedCategory]);
+
+  const handleValueChange = (itemValue) => {
+    const selectedStorage = storageMethods.filter(
+      (item) => item.storageName == itemValue
+    )[0];
+    setSelectedStorage(selectedStorage);
+    console.log(selectedStorage);
+  };
+
+  return (
+    <Box w="3/4" maxW="300">
+      <FormControl isRequired>
+        <Select
+          minWidth="200"
+          placeholder="Choose a storage method"
+          _selectedItem={{
+            bg: 'teal.600',
+          }}
+          mt={1}
+          onValueChange={(itemValue) => {
+            handleValueChange(itemValue);
+          }}
+          defaultValue={selectedStorage ? selectedStorage.storageName : null}
+        >
+          {selectedCategory
+            ? selectedCategory.storageMethods.map((storageMethod, index) => {
+                return (
+                  <Select.Item
+                    label={storageMethod.storageName}
+                    value={storageMethod.storageName}
+                    key={storageMethod.storageName + index}
+                  />
+                );
+              })
+            : selectedShelfLifeItem.storageName}
+        </Select>
+      </FormControl>
+    </Box>
+  );
 }
