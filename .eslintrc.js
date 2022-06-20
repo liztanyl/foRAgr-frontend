9345f2549a7bf46da5235737459a5f69b1186f1a
gcskhor <gcskhor@yahoo.com.sg>
2022-06-20 14:43:10 +0800

    undo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-  // env: {
-  //   browser: true,
-  //   es2021: true,
-  // },
-  // extends: ['plugin:react/recommended', 'airbnb'],
-  // parserOptions: {
-  //   ecmaFeatures: {
-  //     jsx: true,
-  //   },
-  //   ecmaVersion: 12,
-  //   sourceType: 'module',
-  // },
-  // plugins: ['react'],
-  // rules: {
-  //   'brace-style': 'off',
-  //   'import/extensions': ['error', 'ignorePackages'],
-  //   'import/no-extraneous-dependencies': 'off',
-  //   'linebreak-style': 'off',
-  //   'no-console': 'off',
-  //   'no-param-reassign': ['error', { props: false }],
-  //   'import/no-named-as-default': 0,
-  //   'import/no-named-as-default-member': 0,
-  //   'no-unused-expressions': ['error', { allowTernary: true }],
-  //   'function-paren-newline': ['error', 'never'],
-  //   'react/prop-types': [0],
-  // },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'brace-style': 'off',
+    'import/extensions': ['error', 'ignorePackages'],
+    'import/no-extraneous-dependencies': 'off',
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+    'no-unused-expressions': ['error', { allowTernary: true }],
+    'function-paren-newline': ['error', 'never'],
+    'react/prop-types': [0],
+  },
 };
