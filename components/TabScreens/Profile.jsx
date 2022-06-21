b9cf1df892c37118e01525b39d5d3e0121988a1e
gcskhor <gcskhor@yahoo.com.sg>
2022-06-21 11:40:01 +0800

    added login/profile page

---
+++ components/TabScreens/Profile.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { VStack, Button } from 'native-base';
+import { View } from 'react-native';
+import axios from 'axios';
+import { BACKEND_URL } from '../../store.js';
+
+export default function Profile() {
+  const handleLogin = () => {
+    console.log('logging in');
+    axios
+      .get(`${BACKEND_URL}/user/getGoogleAuthUrl`)
+      .then((response) => {
+        console.log(response.data);
+        const redirectURI = response.data;
+        window.location.href = redirectURI;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <VStack space={3} alignItems="center">
+        <Button
+          size="lg"
+          padding={10}
+          colorScheme="primary"
+          onPress={handleLogin}
+        >
+          Login
+        </Button>
+        <Button size="lg" padding={10} colorScheme="secondary">
+          Logout
+        </Button>
+      </VStack>
+    </View>
+  );
+}
