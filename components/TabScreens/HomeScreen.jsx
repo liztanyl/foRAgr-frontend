b0a3fa872c9d1b9e04c755c5d94aad3df080b738
gcskhor <gcskhor@yahoo.com.sg>
2022-06-24 09:14:35 +0800

    conditional statements adjustment to attempt to fix userDetail bugs

--- components/TabScreens/HomeScreen.jsx
+++ components/TabScreens/HomeScreen.jsx
@@ -3,26 +3,36 @@ import moment from 'moment';
 import { View } from 'react-native';
 import { Heading } from 'native-base';
 import { useFridgeContext } from '../FridgeContext.jsx';
+import { useUserContext } from '../UserContext.jsx';
 import StorageNavigation from '../HomeScreenComponents/StorageNavigation.jsx';
 import ItemDisplay from '../HomeScreenComponents/ItemDisplay.jsx';
 import ExpiryAlert from '../HomeScreenComponents/ExpiryAlert.jsx';
-import { STORAGE, SORT } from '../HomeScreenComponents/helpers.js';
+import { STORAGE, SORT, isEmpty } from '../HomeScreenComponents/helpers.js';
 import allowsNotificationsAsync from '../NotificationComponent/allowsNotificationsAsync.js';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   const [currentStorage, setCurrentStorage] = useState(STORAGE.ALL);
   const [sortBy, setSortBy] = useState(SORT.EXPIRY_ASC);
   const [numExpiringItems, setNumExpiringItems] = useState(null);
   const { fridgeItems } = useFridgeContext();
+  const { userDetails } = useUserContext();
+
   allowsNotificationsAsync();
   useEffect(() => {
-    if (fridgeItems && fridgeItems.length > 0) {
-      const expiring = fridgeItems?.filter((item) => (moment(item.expiryDate).diff(new Date(), 'days') < 3));
+    console.log('\n\n\nfridgeItems');
+    console.log(fridgeItems);
+    console.log('\n\n\nuserDetails');
+    console.log(userDetails);
+    if (isEmpty(userDetails)) navigation.navigate('Profile');
+    else if (!isEmpty(userDetails) && fridgeItems && fridgeItems.length > 0) {
+      const expiring = fridgeItems?.filter(
+        (item) => moment(item.expiryDate).diff(new Date(), 'days') < 3
+      );
       console.log(expiring);
       if (expiring.length > 0) setNumExpiringItems(expiring.length);
       else setNumExpiringItems(null);
     }
-  }, [fridgeItems]);
+  }, [fridgeItems, userDetails]);
 
   return (
     <View style={{ height: '100%' }}>
@@ -33,11 +43,10 @@ export default function HomeScreen() {
         sortBy={sortBy}
         setSortBy={setSortBy}
       />
-      <Heading px="4" fontWeight="700" fontSize="2xl">{currentStorage}</Heading>
-      <ItemDisplay
-        currentStorage={currentStorage}
-        sortBy={sortBy}
-      />
+      <Heading px="4" fontWeight="700" fontSize="2xl">
+        {currentStorage}
+      </Heading>
+      <ItemDisplay currentStorage={currentStorage} sortBy={sortBy} />
     </View>
   );
 }
