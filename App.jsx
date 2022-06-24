82b5bd83d0fc40bb5b168d2829a075569f2f66bb
gcskhor <gcskhor@yahoo.com.sg>
2022-06-24 18:03:12 +0800

    - added ErrorBoundary Component - added line to ignore yellow log boxes

--- App.jsx
+++ App.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import axios from 'axios';
 import * as Notifications from 'expo-notifications';
 import { NativeBaseProvider, extendTheme } from 'native-base';
+import { LogBox } from 'react-native';
 import { UserContextProvider } from './components/UserContext.jsx';
+
 import Main from './components/Main.jsx';
 import getTheme from './components/styles/theme.jsx';
+import ErrorBoundary from './components/ErrorBoundary.jsx';
 
 axios.defaults.withCredentials = true;
 Notifications.setNotificationHandler({
@@ -14,15 +17,18 @@ Notifications.setNotificationHandler({
     shouldSetBadge: false,
   }),
 });
+LogBox.ignoreAllLogs();
 
 export default function App() {
   const newTheme = extendTheme(getTheme());
 
   return (
     <NativeBaseProvider theme={newTheme}>
-      <UserContextProvider>
-        <Main />
-      </UserContextProvider>
+      <ErrorBoundary>
+        <UserContextProvider>
+          <Main />
+        </UserContextProvider>
+      </ErrorBoundary>
     </NativeBaseProvider>
   );
 }
