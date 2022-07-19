3becf42e6b649f87d0d21d72b4830cf365bcdef8
liztanyl <elizabeth.tanyulin@gmail.com>
2022-07-19 13:08:05 +0800

    update url for deployed backend

--- store.js
+++ store.js
@@ -1,4 +1,2 @@
-import { myIpAddress } from './secret.js';
-
 // eslint-disable-next-line import/prefer-default-export
-export const BACKEND_URL = process.env.BACKEND_URL || `http://${myIpAddress}:3004`;
+export const BACKEND_URL = 'https://rocky-inlet-29806.herokuapp.com';
